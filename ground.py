48c1dc0ed07298f11da0a2fd9495cf4ef74e2fa4
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2021-01-08 19:18:15 -0800

    Create ground

--- ground.py
+++ ground.py
@@ -5,6 +5,12 @@ import random
 from tkinter import filedialog
 from tkinter import *
 
+from game import displaysurface
 class Ground(pygame.sprite.Sprite):
   def __init__(self):
     super().__init__()
+    self.image = pygame.image.load("Ground.png")
+    self.rect = self.image.get_rect(center = (350, 350))
+
+  def render(self):
+    displaysurface.blit(self.image, (self.rect.x, self.rect.y))

a2ea3635d6357491b445d3cb420354b6fdee12f9
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2021-01-10 15:09:30 -0800

    Optimize gravity_check

--- sprites/player.py
+++ sprites/player.py
@@ -50,6 +50,16 @@ class Player(pygame.sprite.Sprite):
       self.pos.x = WIDTH
 
     self.rect.midbottom = self.pos
+
+  def gravity_check(self, group):
+    hits = pygame.sprite.spritecollide(self, group, False)
+    if self.vel.y > 0:
+      if hits:
+        lowest = hits[0]
+        if self.pos.y < lowest.rect.bottom:
+          self.pos.y = lowest.rect.top + 1
+          self.vel.y = 0
+          self.jumping = False
   
   def update(self):
     pass

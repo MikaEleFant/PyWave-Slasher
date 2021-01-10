02ea21aa057bd3f8b16b6c19ff63dcf16a37dfe1
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2021-01-10 15:02:55 -0800

    Debug gravity issues

--- game.py
+++ game.py
@@ -21,7 +21,19 @@ player = Player()
 ground_group = pygame.sprite.Group()
 ground_group.add(ground)
 
+def gravity_check(entity):
+  hits = pygame.sprite.spritecollide(entity, ground_group, False)
+  if entity.vel.y > 0:
+    if hits:
+      lowest = hits[0]
+      if entity.pos.y < lowest.rect.bottom:
+        entity.pos.y = lowest.rect.top + 1
+        entity.vel.y = 0
+        entity.jumping = False
+
 while True:
+  gravity_check(player)
+
   for event in pygame.event.get():
     if event.type == QUIT:
       pygame.quit()
